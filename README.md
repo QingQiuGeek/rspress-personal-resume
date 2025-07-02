558c9af5bda7112626a1d12c5cbba46428ae5ca3
QingQiu <3343759238@qq.com>
2025-07-02 12:23:19 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# rspressDemo
-SSG框架rspress
+# rspress-personal-resume
+
+基于 SSG 框架 rspress 搭建的个人简历页面
--- docs/components/project.tsx
+++ docs/components/project.tsx
@@ -1,33 +1,33 @@
-import { Button, Card, Flex, Typography } from 'antd';
+import React from 'react';
+import { Carousel } from 'antd';
+
+const contentStyle: React.CSSProperties = {
+	margin: 0,
+	height: '160px',
+	color: '#fff',
+	lineHeight: '160px',
+	textAlign: 'center',
+	background: '#364d79',
+};
+
 export function Project() {
-	return (
-		<Card
-			hoverable
-			styles={{ body: { padding: 0, overflow: 'hidden' } }}
-		>
-			<Flex justify='space-between'>
-				<img
-					srcSet='/boy1.jpg'
-					alt='avatar'
-					style={{ display: 'block', width: 180 }}
-				/>
-				<Flex
-					vertical
-					align='flex-end'
-					justify='space-between'
-					style={{ padding: 32 }}
-				>
-					<Typography.Title level={4}>
-						独立原创，docker部署，前端TS、AntDesign、umiMAX，后端SpringBoot
-					</Typography.Title>
-					<Button
-						href='#'
-						target='_blank'
-					>
-						Go to
-					</Button>
-				</Flex>
-			</Flex>
-		</Card>
-	);
+	// const onChange = (currentSlide: number) => {
+	// 	console.log(currentSlide);
+	// };
+	// return (
+	// 	<Carousel afterChange={onChange}>
+	// 		<div>
+	// 			<h3 style={contentStyle}>1</h3>
+	// 		</div>
+	// 		<div>
+	// 			<h3 style={contentStyle}>2</h3>
+	// 		</div>
+	// 		<div>
+	// 			<h3 style={contentStyle}>3</h3>
+	// 		</div>
+	// 		<div>
+	// 			<h3 style={contentStyle}>4</h3>
+	// 		</div>
+	// 	</Carousel>
+	// );
 }
